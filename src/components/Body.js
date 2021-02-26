c4dfa9b2aef1a066081757e540cbe7643ac2696d
Casimiro <cp.ciancimino@gmail.com>
2021-02-26 18:37:59 +0100

    add: button to products section

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,35 @@
 import { Component } from 'react';
 import './Body.css';
+import Product from './Product';
 
 class Body extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      isProductVisibile: false,
+    }
+  }
+  
+  showProducts() {
+    this.setState({ isProductVisibile: true });
+  }
+
   render() {
-    const { cover, title, description } = this.props;
-    
-    return (
-      <main>
-        <img src={ cover } alt={ title }/>
-        <h1>{ title }</h1>
-        <h2>{ description }</h2>
+    const { data, cover, title, description } = this.props;
+
+    return (  
+      <main className="Body">
+      { 
+        this.state.isProductVisibile ?
+        <div className="productsWrapper"> <Product products={ data.products }/> </div> :
+        <>
+          <img src={ cover } alt={ title } className="main--img"/>
+          <button className="showProductsBtn" onClick={ () => this.showProducts() }>Show Products!</button>
+          <h1>{ title }</h1>
+          <h2>{ description }</h2>      
+        </>
+      }
       </main>
     )
   }
